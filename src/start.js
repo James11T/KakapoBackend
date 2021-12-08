502c2ccd1d1bfcb815b7cfdc1f64a2a8b45dbc9e
James11T <jamail090999@gmail.com>
2021-12-08 19:24:17 +0000

    cleanup

--- src/start.js
+++ src/start.js
@@ -18,10 +18,6 @@ const startApp = async (apiBase, port = process.env.API_PORT) => {
   const db = new DBWrapper();
   global.db = db;
 
-  /*let tableSuccess = models.reduce(async (prev, curr) => {
-    return prev && (await db.registerTable(curr));
-  }, true);*/
-
   // Register all tables with the database
   // Create them if they dont exist
   let tableSuccess = db.registerTables(models);
