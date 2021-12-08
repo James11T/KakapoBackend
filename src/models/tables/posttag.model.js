63b5238f5f7bb8d642fd6ade9965643e19b9a875
James11T <jamail090999@gmail.com>
2021-12-08 19:23:57 +0000

    fixed typos

--- src/models/tables/posttag.model.js
+++ src/models/tables/posttag.model.js
@@ -4,8 +4,8 @@ import { NumberField, StringField } from "../fields.js";
 const model = (db) =>
   new Table("post_tag", db, {
     id: new NumberField("id", { autoIncrement: true, primaryKey: true, sensitivity: -1 }),
-    tag: new StringField("kakapo_id", { maxLength: 64, sensitivity: 0 }),
-    post_id: new StringField("post_id", { sensitivity: 0, reference: { table: "post", field: "id" } }),
+    tag: new StringField("tag", { maxLength: 64, sensitivity: 0 }),
+    post: new NumberField("post", { sensitivity: 0, reference: { table: "post", field: "id" } }),
   });
 
 export default model;
