3f1568428a59cdbc64aecbbd977618db0d0de02f
James11T <jamail090999@gmail.com>
2021-12-08 18:54:30 +0000

    fixed comment typo

--- src/models/tables/comment.model.js
+++ src/models/tables/comment.model.js
@@ -5,7 +5,7 @@ const model = (db) =>
   new Table("comment", db, {
     id: new NumberField("id", { autoIncrement: true, primaryKey: true, sensitivity: -1 }),
     post: new NumberField("post", { sensitivity: 0, reference: { table: "post", field: "id" } }),
-    content: new StringField("liker", { maxLength: 256, sensitivity: 0 }),
+    content: new StringField("content", { maxLength: 256, sensitivity: 0 }),
     commented_at: new NumberField("commented_at", { sensitivity: 0 }),
     author: new NumberField("author", { sensitivity: 0, reference: { table: "user", field: "id" } }),
     edited: new BooleanField("edited", { defaultValue: false, sensitivity: 0 }),
