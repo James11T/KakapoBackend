921e083e248d1081c560e7b09e79b95d17312c73
James11T <jamail090999@gmail.com>
2021-12-08 15:21:45 +0000

    added comment endpoints

--- src/models/tables/comment.js
+++ src/models/tables/comment.js
@@ -8,6 +8,7 @@ const model = (db) =>
     content: new StringField("liker", { maxLength: 256, sensitivity: 0 }),
     commented_at: new NumberField("commented_at", { sensitivity: 0 }),
     author: new NumberField("author", { sensitivity: 0, reference: { table: "user", field: "id" } }),
+    edited: new BooleanField("edited", { defaultValue: false }),
     public_id: new StringField("public_id", { maxLength: 16, sensitivity: 0 }),
   });
 
--- src/routes/post/comment.js
+++ src/routes/post/comment.js
@@ -4,7 +4,7 @@ import {
   MissingParametersError,
   CommentNotFoundError,
   BadParametersError,
-  NotCommentOwnerError
+  NotCommentOwnerError,
 } from "../../errors/apierrors.js";
 import { checkRequiredParameters, getEpoch, generatePublicId } from "../../utils.js";
 import { isAuthenticated } from "../../middleware/auth.js";
@@ -66,8 +66,8 @@ const addPostComment = async (req, res) => {
     id: newComment.public_id,
     content: newComment.content,
     commented_at: newComment.commented_at,
-    post: req.post.public_id
-  }
+    post: req.post.public_id,
+  };
 
   return res.send({ success: true, comment: commentExport });
 };
@@ -76,11 +76,69 @@ const deletePostComment = async (req, res) => {
   if (req.user.id !== req.comment.author.id) {
     return sendError(res, new NotCommentOwnerError());
   }
+
+  const [deleteCommentError, deleteCommentResult] = await globa.db.table("comment").delete({ id: req.comment.id });
+  if (deleteCommentError) {
+    return sendError(res, new GenericError());
+  }
+
+  return res.send({ success: true });
 };
 
-const editPostComment = async (req, res) => {};
+const editPostComment = async (req, res) => {
+  if (req.user.id !== req.comment.author.id) {
+    return sendError(res, new NotCommentOwnerError());
+  }
 
-const getCommentsInRange = async (req, res) => {};
+  const [hasRequiredParameters, missingParameters] = checkRequiredParameters(req.body, ["content"]);
+  if (!hasRequiredParameters) {
+    return sendError(res, new MissingParametersError({ missingParameters: missingParameters }));
+  }
+
+  const { content } = req.body;
+  let trimmedContent = content.trim();
+  if (trimmedContent.length === 0) {
+    return sendError(res, new BadParametersError({ badParameters: ["content"] }));
+  }
+
+  const [editCommentError, editCommentResult] = await global.db
+    .table("comment")
+    .edit({ id: req.comment.id }, { content: trimmedContent });
+  if (editCommentError) {
+    return sendError(res, new GenericError());
+  }
+
+  return res.send({ success: true, content: trimmedContent });
+};
+
+const getCommentsInRange = async (req, res) => {
+  const [hasRequiredParameters, missingParameters] = checkRequiredParameters(req.body, ["from", "count"]);
+  if (!hasRequiredParameters) {
+    return sendError(res, new MissingParametersError({ missingParameters: missingParameters }));
+  }
+
+  let { from = 0, count = 10 } = req.body;
+
+  try {
+    from = Number(from);
+    count = Number(count);
+  } catch (castingError) {
+    // Provided parameters are not numbers
+    return sendError(res, new BadParametersError());
+  }
+
+  count = clamp(count, 1, 50);
+  from = Math.max(from, 0); // Minimum 0
+
+  const [queryError, queryResults] = await global.db.table("comment").limit("*", { post: req.post.id }, from, count);
+  if (queryError) {
+    return sendError(res, new GenericError());
+  }
+
+  return res.send({
+    comments: queryResults.map((comment) => global.db.table("comment").filter(comment, 0)),
+  });
+};
 
 const getPostCommentRoutes = () => {
   const router = express.Router();
