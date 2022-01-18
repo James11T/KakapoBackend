f6af059570c81c9925384bcbdd21d213aade8c17
James <jamail090999@gmail.com>
2022-01-18 13:19:45 +0000

    removed old ORM

--- src/models/tables/index.js
+++
@@ -1,9 +0,0 @@
-import userModel from "./user.model.js";
-import postModel from "./post.model.js";
-import friendRequestModel from "./friendrequest.model.js";
-import postTag from "./posttag.model.js";
-import friendshipModel from "./friendship.model.js";
-import likeModel from "./like.model.js";
-import commentModel from "./comment.model.js";
-
-export default [userModel, friendRequestModel, friendshipModel, postModel, postTag, likeModel, commentModel];
